27508b84f896abb4526d738e603c1ea9b14ca881
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-11-25 11:41:02 +0900

    HttpClientによるCRUD操作

---
+++ example/example6.js
@@ -0,0 +1,22 @@
+const { Observable } = require("rxjs");
+const { map } = require("rxjs/operators");
+
+const responseData = {
+  data: {
+    articles: [
+      { id: 1, text: "Article 1" },
+      { id: 2, text: "Article 2" },
+      { id: 3, text: "Article 3" },
+    ],
+  },
+};
+
+const observable = Observable.create((observer) => {
+  observer.next(responseData);
+  observer.complete();
+});
+observable.pipe(map((res) => res.data.articles)).subscribe(
+  (data) => console.log(data),
+  (err) => console.log(err),
+  () => console.log("complete")
+);
