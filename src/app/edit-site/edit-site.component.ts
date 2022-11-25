bbfcabbb12e1c139c402ee4fc02556125b7c298a
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-11-25 14:31:39 +0900

    EditSite component

--- src/app/edit-site/edit-site.component.ts
+++ src/app/edit-site/edit-site.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Site } from '../site.model';
+import { SiteService } from '../site.service';
 
 @Component({
   selector: 'app-edit-site',
   templateUrl: './edit-site.component.html',
-  styleUrls: ['./edit-site.component.css']
+  styleUrls: ['./edit-site.component.css'],
 })
 export class EditSiteComponent implements OnInit {
+  siteName: string = '';
+  siteURL: string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private siteService: SiteService
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.siteService.getSite(id!).subscribe((data: Site) => {
+      this.siteName = data.name;
+      this.siteURL = data.url;
+    });
   }
 
+  editSite(): void {
+    if (!this.siteName.trim() || !this.siteURL.trim()) return;
+
+    const id = this.route.snapshot.paramMap.get('id');
+    const siteInfo: Site = {
+      name: this.siteName,
+      url: this.siteURL,
+    };
+    this.siteService.updateSite(id!, siteInfo).subscribe(() => {
+      this.siteName = '';
+      this.siteURL = '';
+      this.router.navigate(['/']);
+    });
+  }
 }
